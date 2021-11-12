2843c0152317a55172f5076eb2218d0b118afb6e
wjlee-mathpang <dev.woojin.lee@gmail.com>
2021-11-12 22:52:37 +0900

    add login and signup template

--- src/components/pages/signin/signInPage.tsx
+++ src/components/pages/signin/signInPage.tsx
@@ -1,10 +1,61 @@
 import React from "react";
 import styled from "styled-components";
+import { Container, TextField, Typography, Box, Button } from "@mui/material";
+import wichuIcon from "../../../assets/icon/wichu-icon.png";
 
-export const StyledSignInPage = styled.div``;
+const StyledSignInPage = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  padding: 50px 20px;
+`;
 
-interface PropType {}
+const StyledTextField = styled(TextField)`
+  width: 100%;
 
-export const SignInPage = ({}: PropType): JSX.Element | null => {
-  return <StyledSignInPage></StyledSignInPage>;
+  input {
+    padding: 12px 0;
+  }
+`;
+
+const LogoBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto 60px;
+`;
+
+const WichuIcon = styled.img`
+  width: 120px;
+  margin: 0 auto;
+`;
+
+const StyledButton = styled(Button)`
+  && {
+    margin-top: 60px;
+    width: 100%;
+    height: 50px;
+  }
+`;
+
+export const SignInPage = (): JSX.Element | null => {
+  return (
+    <StyledSignInPage maxWidth="sm">
+      <LogoBox>
+        <WichuIcon src={wichuIcon} />
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{ fontWeight: 700, textAlign: "center" }}
+        >
+          wichu
+        </Typography>
+      </LogoBox>
+      <StyledTextField placeholder="아이디" variant="standard" />
+      <StyledTextField
+        placeholder="비밀번호"
+        type="password"
+        variant="standard"
+      />
+      <StyledButton variant="contained">로그인</StyledButton>
+    </StyledSignInPage>
+  );
 };
--- src/components/pages/signup/signUpPage.tsx
+++ src/components/pages/signup/signUpPage.tsx
@@ -1,10 +1,61 @@
 import React from "react";
 import styled from "styled-components";
+import { Container, TextField, Typography, Box, Button } from "@mui/material";
+import wichuIcon from "../../../assets/icon/wichu-icon.png";
 
-export const StyledSignUpPage = styled.div``;
+const StyledSignUpPage = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  padding: 50px 20px;
+`;
 
-interface PropType {}
+const StyledTextField = styled(TextField)`
+  width: 100%;
 
-export const SignUpPage = ({}: PropType): JSX.Element | null => {
-  return <StyledSignUpPage></StyledSignUpPage>;
+  input {
+    padding: 12px 0;
+  }
+`;
+
+const LogoBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto 60px;
+`;
+
+const WichuIcon = styled.img`
+  width: 120px;
+  margin: 0 auto;
+`;
+
+const StyledButton = styled(Button)`
+  && {
+    margin-top: 60px;
+    width: 100%;
+    height: 50px;
+  }
+`;
+
+export const SignUpPage = (): JSX.Element | null => {
+  return (
+    <StyledSignUpPage maxWidth="sm">
+      <LogoBox>
+        <WichuIcon src={wichuIcon} />
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{ fontWeight: 700, textAlign: "center" }}
+        >
+          wichu
+        </Typography>
+      </LogoBox>
+      <StyledTextField placeholder="아이디" variant="standard" />
+      <StyledTextField
+        placeholder="비밀번호"
+        type="password"
+        variant="standard"
+      />
+      <StyledButton variant="contained">회원가입</StyledButton>
+    </StyledSignUpPage>
+  );
 };
